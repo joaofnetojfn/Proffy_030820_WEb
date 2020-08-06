47535420b41b6b089722d589b99043c32d46ce0c
joaofnetojfn <joaofnetojfn@gmail.com>
2020-08-05 23:37:06 -0300

    Final do Front_End Desktop_Responsivo - Flta somente ligar o serviço Nodejs, já criados

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,11 +1,112 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import warningIcon from '../../assets/images/icons/warning.svg';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
+
+import './styles.css';
+
 
 function TeacherForm() {
+    const [scheduleItems, setScheduleItems] = useState([
+        { week_day: 0, from: '', to: ''},
+    ]);
+
+    function addNewScheduleItem(){
+        setScheduleItems([
+            ...scheduleItems,
+            { week_day: 0, from: '', to: '' }
+        ]);
+        scheduleItems.push({
+            week_day: 0,
+            from: '',
+            to: ''
+        })
+    }
+
     return (
-        <div id="page-teacher-list" className="container">
-            <PageHeader title=" Que incrível que você quer dar aulas." />
+        <div id="page-teacher-form" className="container">
+            <PageHeader
+             title=" Que incrível que você quer dar aulas." 
+             description=" O primeiro passo é preencher esse formulário de inscrição"
+            />
+
+             <main>
+             <fieldset>
+                    <legend> Seus Dados</legend>
+                    <Input name="name" label="Nome completo"/>
+                    <Input name="avatar" label="Avatar"/>
+                    <Input name="whatsapp" label="Whatsapp"/>
+                    <Textarea name="bio" label="Biografia"/>
+
+                </fieldset>
+
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+                    <Select
+                        name="subject" 
+                        label="Matéria"
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Ciências', label: 'Ciências' },
+                            { value: 'Educação física', label: 'Educação física' },
+                            { value: 'Física', label: 'Física' },
+                            { value: 'Geografia', label: 'Geografia' },
+                            { value: 'História', label: 'História' },
+                            { value: 'Matemática', label: 'Matemática' },
+                            { value: 'Porguês', label: 'Porguês' },
+                            { value: 'Química', label: 'Química' },
+                            { value: 'Filosifia', label: 'Filosifia' },
+                            { value: 'Literatura', label: 'Literatura' },
+                        ]}
+                    />
+                    <Input name="cost" label="Custo da sua hora por aula"/>
+
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        Horários Disponíveis
+                        <button type="button" onClick={addNewScheduleItem}>
+                            + Novo horário
+                        </button>
+                    </legend>
+                    {scheduleItems.map(scheduleItem => {
+                        return (
+                            <div key={scheduleItem.week_day} className="schedule-item">
+                                <Select
+                                    name="week_day" 
+                                    label="Dia da semana"
+                                    options={[
+                                        { value: '0', label: 'Domingo' },
+                                        { value: '1', label: 'Segunda-feira' },
+                                        { value: '2', label: 'Terça-feira' },
+                                        { value: '3', label: 'Quarta-feira' },
+                                        { value: '4', label: 'Quinta-feira' },
+                                        { value: '5', label: 'Sexta-feira' },
+                                        { value: '6', label: 'Sábado' },
+                                    ]}
+                                />
+                                <Input name="from" label="Das" type="time" />
+                                <Input name="to" label="Até" type="time" />
+                            </div>
+                        );
+                    })}
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso Importante"/>
+                        Importante! <br />
+                        Preencha todos os dados
+                    </p>
+                    <button type="button">Salvar cadastro </button>
+                </footer>
+             </main>
         </div>
+        
     );
 }
 
